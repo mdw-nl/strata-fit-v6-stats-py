e2ace9ce89b1f1feac023ddfe5cf90fe8f842fab
emdeeweegio <ivan.zhovannik@medicaldataworks.nl>
2025-04-03 18:04:02 +0200

    removed kwargs for clarity

--- v6_strata_fit_stats_py/__init__.py
+++ v6_strata_fit_stats_py/__init__.py
@@ -5,7 +5,7 @@ from vantage6.algorithm.tools.decorators import data
 from .logic import compute_partial_stats
 
 @data(1)
-def partial_stats(df: pd.DataFrame, **kwargs):
+def partial_stats(df: pd.DataFrame):
     
     results = compute_partial_stats(df)
     info(results)
